f8ee7398aef97ca74891d19f3dff06cffecc5350
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-20 12:02:23 -0500

    housekeeping

--- src/tests/mocha.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Suite } from 'mocha'
-
-import computeMinutesWorkedUnit from '../modules/timeCards/computeMinutesWorked.spec'
-
-describe('Server unit testing', function (this: Suite) {
-  describe('ComputeTotalHoursUnit', computeMinutesWorkedUnit)
-})
