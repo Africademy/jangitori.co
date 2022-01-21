ac0773732fc76ef78c80e25c9b41ebc1df2610da
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-21 14:52:24 -0500

    fix

--- src/modules/dashboard/DashboardStore.ts
+++ src/modules/dashboard/DashboardStore.ts
@@ -9,11 +9,7 @@ import { BaseTabKey, getDashboardTabsForRole, getTabKeyForIndex } from './tabs'
 
 export default class DashboardStore<TabKey extends BaseTabKey = BaseTabKey> {
   tabIndex = 0
-  timesheetDetailsQuery: TimesheetDetailsQuery | null = null
-
-  clearQueries() {
-    this.timesheetDetailsQuery = null
-  }
+  timesheetDetailsQuery: TimesheetDetailsQuery | Falsy = null
 
   get tabKeys(): TabKey[] {
     const role = this.root.getAccountRole()
@@ -31,20 +27,21 @@ export default class DashboardStore<TabKey extends BaseTabKey = BaseTabKey> {
         '❗ WARNING: Router is not ready but called DashboardStore.routeTo()',
       )
     }
+
     const role = this.root.getAccountRole()
     const tabKeys = this.tabKeys
     const tab = value
 
     const filterUrl = filter ? `?payPeriodEnd=${filter.payPeriodEnd}` : ``
+    this.timesheetDetailsQuery = filter
     if (typeof tab === 'number') {
       this.tabIndex = tab
-      this.clearQueries()
+
       Router.router?.push(
         routes.dashboardPage(role, getTabKeyForIndex(tab, tabKeys) + filterUrl),
       )
     } else {
       this.tabIndex = tabKeys.indexOf(tab)
-      this.clearQueries()
       Router.router?.push(routes.dashboardPage(role, tab) + filterUrl)
     }
   }
