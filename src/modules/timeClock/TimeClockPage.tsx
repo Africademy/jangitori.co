42d6b09d822fdf286f50b31dc7fb524ca7177107
Lesley Chang <lesleychang@Lesleys-MacBook-Pro.local>
2022-01-23 09:56:18 -0500

    fix

--- src/modules/timeClock/TimeClockPage.tsx
+++ src/modules/timeClock/TimeClockPage.tsx
@@ -1,3 +1,33 @@
-import { TimeClock } from './TimeClock'
+import { observer } from 'mobx-react-lite'
 
-export default TimeClock
+import { AuthenticatedPageProps } from '@/modules/core/types/AuthenticatedPageProps'
+import { ShiftStep } from '@/modules/shifts/shiftStore'
+import { useShiftStore } from '@/modules/stores'
+import LoadingStack from '@/ui/components/LoadingStack'
+import { Redirect } from '@/ui/components/Redirect'
+
+import { StartShift } from './StartShift'
+
+export const employeeRoutes = {
+  timeClock: 'timeClock',
+  overview: 'overview',
+  timesheets: 'timesheets',
+}
+
+const TimeClockPage = observer(function TimeClock({
+  user: employee,
+}: AuthenticatedPageProps) {
+  const shiftStore = useShiftStore()
+
+  if (shiftStore.step === ShiftStep.Initializing) return <LoadingStack />
+
+  if (shiftStore.step === ShiftStep.Idle)
+    return <StartShift employee={employee} />
+
+  if (shiftStore.step === ShiftStep.ClockedIn)
+    return <Redirect to={employeeRoutes.timeClock} />
+
+  return <Redirect to={employeeRoutes.overview} />
+})
+
+export default TimeClockPage
